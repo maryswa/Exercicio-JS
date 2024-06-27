4efe706f6e55e994c4a7aeb42b07efa520809bd5
maryswa <134395549+maryswa@users.noreply.github.com>
2024-06-27 07:45:24 -0300

    Add files via upload

--- aula 3/sketch.js
+++ aula 3/sketch.js
@@ -1,21 +1,29 @@
-//funçao metrica exemplo usando rotaçao
+//Usando a funçao map com redianos.
+//Usando a funçao map para mapear o angulo de rotaçao em radianos
+//implementado a velocidade e a aceleraçao angular
+//A funçao map usa cinco parametros
+//Valor inicial (posiçao atual do mause) e um intervelo inicial(posiçao 0 e width da tela)e um intervalo final (edido em radianos).
 
 let angulo =0;
+let anguloV =0;
+let anguloA =0;
 
 function setup() {
   createCanvas(400, 400);
-  angleMode(DEGREES);
+  angleMode(RADIANS);
 }
 
 function draw() {
-  angulo = map(mouseX,0,width,0,360);
+  let anguloA = map(mouseX,0,width,-0.01,0.01);
   
-  background(146,83,157);
+  background(14,83,20);
   noStroke();
-  fill(440,99,165);
-  rectMode(CENTER)
+  fill(13,200,16);
+  rectMode(CENTER);
   translate(200,200);
   rotate(angulo);
   rect(0,0,256,32);
   
+  angulo += anguloV;
+  anguloV += anguloA;
 }
